39ba38e3847fbb2c17eb8863c1aafbfe1a101174
yogaperdana2308 <yoga.perdana2308@gmail.com>
2025-10-16 14:25:18 +0700

    Add files via upload

---
+++ lib/tugas1_flutter/tugas5.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+
+class Tugas5 extends StatefulWidget {
+  const Tugas5({super.key});
+
+  @override
+  State<Tugas5> createState() => _Tugas5State();
+}
+
+class _Tugas5State extends State<Tugas5> {
+  bool showName = false;
+  bool showFavorite = false;
+  bool showAll = false;
+  num counter = 0;
+  bool showKotak = false;
+  double counter1 = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            counter--;
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+      backgroundColor: const Color(0xffFBF9D1),
+      appBar: AppBar(
+        title: Text(
+          "Tugas 5 Flutter",
+          style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+        ),
+        backgroundColor: Color(0xff9A3F3F),
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (showName) ...[
+              Text(
+                "Yoga Perdana Ferdiansyah",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: const Color.fromARGB(255, 0, 0, 0),
+                ),
+              ),
+              SizedBox(height: 4),
+              TextButton(
+                onPressed: () {
+                  print("Tampilkan Nama : $showName");
+                  showName = !showName;
+                  setState(() {});
+                },
+                child: Text(showName ? "Sembunyikan Nama" : "Tampilkan Nama"),
+              ),
+            ] else ...[
+              SizedBox(height: 4),
+              TextButton(
+                onPressed: () {
+                  print("Tampilkan Nama : $showName");
+                  showName = !showName;
+                  setState(() {});
+                },
+                child: Text(showName ? "Sembunyikan Nama" : "Tampilkan Nama"),
+              ),
+            ],
+            IconButton(
+              onPressed: () {
+                showFavorite = !showFavorite;
+                setState(() {});
+              },
+              icon: Icon(
+                showFavorite ? Icons.favorite : Icons.favorite_border_outlined,
+                color: Colors.red,
+              ),
+            ),
+            Text(showFavorite ? "Disukai" : " "),
+            SizedBox(
+              child: Column(
+                children: [
+                  Text(
+                    showAll
+                        ? 'inilsjdksjdgkjhsbkgjhbsd skhfdbgkhsfkhbsdkghjbsdkgjhsbdkdf sfglnsdgnsjdfngsfg sdfkjhsdfjlhsdf'
+                        : '    ',
+                    maxLines: 3,
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      showAll = !showAll;
+                      setState(() {});
+                    },
+                    child: Text('Lihat Selengkapnya'),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 100,
+              child: Center(
+                child: Text(counter.toString(), style: TextStyle(fontSize: 24)),
+              ),
+            ),
+
+            InkWell(
+              onTap: () {
+                print('Kotak Sentuh');
+                setState(() {
+                  showKotak = !showKotak;
+                });
+              },
+              child: Container(
+                width: double.infinity,
+                height: 100,
+                color: Color(0xffE6CFA9),
+                child: Center(
+                  child: Text(
+                    showKotak ? '  ' : 'Alhamdulillah ada teks nya',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 0, 0, 0),
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 100),
+            GestureDetector(
+              onDoubleTap: () {
+                print('Ditekan dua kali');
+                setState(() {
+                  counter1 += 2;
+                });
+              },
+              onLongPress: () {
+                print('Tahan Lama');
+                setState(() {
+                  counter1 += 3;
+                });
+              },
+              onTap: () {
+                print('ditekan sekali');
+                setState(() {
+                  counter1++;
+                });
+              },
+              child: Text('Tekan Aku'),
+            ),
+            Center(child: Text('Jumlahnya : $counter1')),
+          ],
+        ),
+      ),
+    );
+  }
+}
